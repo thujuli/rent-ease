dd2e4efd26de107cfb04e7626746fd2cb9076bcd
Thu Juli <95893131+thujuli@users.noreply.github.com>
2024-10-20 21:26:44 +0800

    Feat/setup common module (#2)
    * feat: setup swagger ui
    
    * Squashed commit of the following:
    
    commit 4618d99727c1721e71f8d758284cb6b0c0334bcc
    Author: Thu Juli <95893131+thujuli@users.noreply.github.com>
    Date:   Wed Oct 16 04:47:01 2024 +0800
    
        feat: setup prisma with seeder (#1)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '~/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Rent Ease API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, documentFactory);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port);
 }
 bootstrap();
